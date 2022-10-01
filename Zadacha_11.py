1f0aa101d63608651dffcec965a67acebb6578ee
Admin <tmurzagaleev@yandex.ru>
2022-10-01 22:26:20 +0700

    Решение задачи 11.2 из ДЗ

--- Zadacha_11.py
+++ Zadacha_11.py
@@ -2,19 +2,12 @@
 # Пример:
 # - 6782 -> 23
 # - 0,56 -> 11
-n = float(input(
-    'Введите вещественное число со количеством знаков после запятой не более 4: '))
-myNumber = 0
-i = 0
-while n % 10 != 0:
-    n = n*10
-    i += 1
-    if i > 4:  # данное условие необходимо для верного решения
-        break
-myNumber = int(n//10)
-print(myNumber)
-sumNumber = 0
-while myNumber > 0:
-    sumNumber = sumNumber+myNumber % 10
-    myNumber = myNumber // 10
-print(f'Сумма составляющих число цифр с 4 знаками после запятой равно {sumNumber}')
+
+n = input("Введите любое вещественное число: ").replace('.', '').replace('-', '')
+while not n.isdigit():
+    n = input("ВВведите любое вещественное число: ").replace('.', '').replace('-', '')
+    
+my_sum = 0
+n = list(n)
+my_sum = sum(list(map(int,n)))
+print(f'Сумму цифр веденного числа равна {my_sum}')
